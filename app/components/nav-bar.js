a4aa61248d73bf812351f97e8da42dce779ff18d
Andrew Cheung <ac1493@yp.com>
2017-09-26 11:06:16 -0700

    Fixed navbar highlights.

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didRender() {
+	var selected;
+    Ember.$('ul.nav > li.navbar-button a').each(function () {
+        if (Ember.$(this).attr("href").endsWith(location.hash)) {
+            var parent = Ember.$(this).closest('ul.nav > li.navbar-button');
+            parent.addClass('open');
+            selected = parent;
+            Ember.$(this).addClass('selected');
+        }
+    });
+
+    Ember.$('ul.nav li.navbar-button').click(function(){
+        Ember.$('li.navbar-button').not(Ember.$(this)).removeClass('open');
+        Ember.$(this).addClass('open');
+        selected = Ember.$(this);
+    });
+
+    Ember.$('ul.nav > li.navbar-button a').click(function(){
+        Ember.$('li.navbar-button.dropdown a').not(Ember.$(this)).removeClass('selected');
+        Ember.$(this).addClass('selected');
+        //Insert event handling logic
+    });
+    Ember.$('ul.nav > li.navbar-button').hover(function(){
+        // save previously clicked
+        Ember.$('li.navbar-button').not(Ember.$(this)).removeClass('open');
+        Ember.$(this).addClass('open')
+    }, function() {
+        // unsave previously clicked
+        if (selected != null) {
+            Ember.$('ul.nav > li.navbar-button').not(selected).removeClass('open');
+            selected.addClass('open');
+        }
+    });
+  }
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,3 @@
-
-$( document ).ready(function() {
-/*	$("#slides").slidesjs({
-		width: 648,
-		height: 250,
-		//auto: 3000,
-		//continuous: true,
-		//interval: 4000,
-        //autoHeight: true, 
-        pagination: false,
-        //generatePagination: false,
-		//navigation: false
-		play: {
-		  active: false,
-		  effect: "slide",
-		  interval: 8000,
-		  auto: true,
-		  swap: true,
-		  pauseOnHover: false,
-		  restartDelay: 200
-		},
-		navigation: false
-	});
-
-	$(".pop").on("click", function() {
-		$('#imagepreview').attr('src', $(this).attr('expanded')); // here asign the image to the modal when the user click the enlarge link
-		$('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
-	});*/
-});
-
 function addHash(str) {
     return str.startsWith("#")?str:"#"+str;
 }
@@ -56,7 +26,3 @@ $("#contact_form").submit(function(e) {
 
     e.preventDefault(); // avoid to execute the actual submit of the form.
 });
-
-/*$(function() {
-    $("img.lazy").lazyload();
-});*/
