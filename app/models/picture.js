02e58e8a2b37f2d8ecffe70d434cd9baf553ddbf
Andrew Cheung <atclate@hotmail.com>
2017-10-20 16:42:16 -0700

    Fix errors on template and model.

--- app/models/picture.js
+++ app/models/picture.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	url: DS.attr('string')
-	album: DS.belongsTo('album');
+	url: DS.attr('string'),
+	album: DS.belongsTo('album')
 });
