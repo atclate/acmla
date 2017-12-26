f13cbbd6fffbf3d1365e45088af17994cba6e154
kcckcc <kelvin.chung@yahoo.com>
2017-12-25 22:42:38 -0800

    Update albums to use Frame
    Update albums to use Frame

--- app/routes/album1.js
+++ app/routes/album1.js
@@ -2,22 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return ['slide-01.jpg', 
-				'slide-02.jpg',
-				'slide-03.jpg',
-				'slide-04.jpg',
-				'slide-05.jpg',
-				'slide-06.jpg',
-				'slide-07.jpg',
-				'slide-08.jpg',
-				'slide-09.jpg',
-				'slide-10.jpg',
-				'slide-11.jpg',
-				'slide-12.jpg',
-				'slide-13.jpg',
-				'slide-14.jpg',
-				'slide-15.jpg',
-				'slide-16.jpg'
+		return ['slide-01.JPG', 
+				'slide-02.JPG',
+				'slide-03.JPG',
+				'slide-04.JPG',
+				'slide-05.JPG',
+				'slide-06.JPG',
+				'slide-07.JPG',
+				'slide-08.JPG',
+				'slide-09.JPG',
+				'slide-10.JPG',
+				'slide-11.JPG',
+				'slide-12.JPG',
+				'slide-13.JPG',
+				'slide-14.JPG',
+				'slide-15.JPG',
+				'slide-16.JPG'
 			];	
 	}
 });
